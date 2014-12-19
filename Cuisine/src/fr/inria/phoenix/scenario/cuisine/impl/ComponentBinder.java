14c217441d4fee792b33dd7aa9d34ff51ddcb8a3
Marc Prats <marc0prats@gmail.com>
2014-12-19 11:34:24 +0100

    Mise en place

---
+++ Cuisine/src/fr/inria/phoenix/scenario/cuisine/impl/ComponentBinder.java
@@ -0,0 +1,10 @@
+package fr.inria.phoenix.scenario.cuisine.impl;
+        
+import fr.inria.phoenix.diasuite.framework.misc.AppComponentBinder;
+
+/* (non-Javadoc)
+ * The binder to provides the various components of the application
+ * @see fr.inria.phoenix.diasuite.framework.misc.AppComponentBinder
+ */
+public class ComponentBinder extends AppComponentBinder {
+}
